7b300a94c53cfb6de73477273ba6aadb82f08de3
MariusAngermann08 <marius.angermann.2018@gmail.com>
2025-02-24 21:30:56 +0100

    Showcase for Jonas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import kivy
 from kivymd.app import MDApp
 
+#Importing custom modules
+from module import HelloWorld
+
 class OpenLinguApp(MDApp):
     #Hallo Jonas
     def build(self, **kwargs):
         return None
 
 if __name__ == "__main__":
+    HelloWorld()
     OpenLinguApp().run()
