6be7aa09b4d4b1bf48fd43702ecb27223dc344ff
MariusAngermann08 <marius.angermann.2018@gmail.com>
2025-02-24 21:19:44 +0100

    Base App Layout

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Wasch dich")
+import kivy
+from kivymd.app import MDApp
+
+class OpenLinguApp(MDApp):
+    def build(self, **kwargs):
+        return None
+
+if __name__ == "__main__":
+    OpenLinguApp().run()
