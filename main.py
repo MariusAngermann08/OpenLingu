a8598e5a25be121fa000cdd8fa0f79e61641a5d8
MariusAngermann08 <marius.angermann.2018@gmail.com>
2025-02-26 10:32:05 +0100

    Quick fix for settings menu error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import kivy
-from kivymd.app import MDApp
+from kivymd.app import MDApp # type: ignore
 from kivy.lang import Builder
 #Importing custom modules
 
@@ -7,6 +7,8 @@ class OpenLinguApp(MDApp):
     #Hallo Jonas
     def build(self, **kwargs):
         return Builder.load_file('kivy\main_ui.kv')
+    def open_settings_menu(self, **kwargs):
+        pass
 
 if __name__ == "__main__":
     
