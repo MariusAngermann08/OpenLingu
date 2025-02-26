be99713d4f27ad35ec4ae644f5a393577c15b564
Ka75erjona <jo.dennler@gmail.com>
2025-02-26 17:14:32 +0100

    Setting Screen implementation

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import kivy
 from kivymd.app import MDApp # type: ignore
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 #Importing custom modules
+Builder.load_file('kivy\main_ui.kv')
+class MainScreen(Screen):
+    pass
 
+class SettingsScreen(Screen):
+    pass
 class OpenLinguApp(MDApp):
     #Hallo Jonas
     def build(self, **kwargs):
-        return Builder.load_file('kivy\main_ui.kv')
+        self.sm = ScreenManager()
+        self.sm.add_widget(MainScreen(name="main"))
+        self.sm.add_widget(SettingsScreen(name="settings"))
+        return self.sm
+    
     def open_settings_menu(self, **kwargs):
-        pass
+        self.sm.current = "settings"
+    def backto_main_menu(self, **kwargs):
+        self.sm.current = "main"
 
 if __name__ == "__main__":
     
