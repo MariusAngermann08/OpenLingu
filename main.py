85dfe322386ffc14ef4fc6012bb7926eb78f72b4
MariusAngermann08 <marius.angermann.2018@gmail.com>
2025-02-26 18:21:55 +0100

    Added settings menu and transition fix

--- main.py
+++ main.py
@@ -11,22 +11,31 @@ class MainScreen(Screen):
 class SettingsScreen(Screen):
     pass
 
+class LanguageScreen(Screen):
+    pass
+
 class OpenLinguApp(MDApp):
     #Hallo Jonas
+    transition_type = "settings"
     def build(self, **kwargs):
         self.sm = ScreenManager()
         self.sm.add_widget(MainScreen(name="main"))
         self.sm.add_widget(SettingsScreen(name="settings"))
+        self.sm.add_widget(LanguageScreen(name="language"))
         return self.sm
     
     def open_settings_menu(self, **kwargs):
+        self.transition_type = "settings"
         self.sm.transition.direction = "left"
         self.sm.current = "settings"
-    def backto_main_menu(self, **kwargs):
-        self.sm.transition.direction = "right"
+    def main_menu(self, **kwargs):
+        if self.transition_type == "settings": self.sm.transition.direction = "right"
+        elif self.transition_type == "language": self.sm.transition.direction = "up"
         self.sm.current = "main"
     def language_menu(self, **kwargs):
-        pass
+        self.transition_type = "language"
+        self.sm.transition.direction = "down"
+        self.sm.current = "language"
 if __name__ == "__main__":
     
     OpenLinguApp().run()
