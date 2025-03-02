c3f7979fcfffd9bc763d13ea639b462267bfcfe0
Ka75erjona <jo.dennler@gmail.com>
2025-03-02 13:41:31 +0100

    Register Ui Setup

--- main.py
+++ main.py
@@ -81,6 +81,53 @@ class OpenLinguApp(MDApp):
         self.sm.current = "signup"
     def welcome_menu(self, **kwargs):
         self.sm.transition.direction = "left"
+class OpenLinguApp(MDApp):
+    # Hallo Jonas
+    transition_type = "settings"
+    db = None
+
+    def __init__(self, **kwargs):
+        super(OpenLinguApp, self).__init__(**kwargs)
+        global APIKEY
+        global URL
+        self.db = Database(apikey=APIKEY, database_url=URL)
+
+    def build(self, **kwargs):
+        self.sm = ScreenManager()
+        self.sm.add_widget(WelcomeScreen(name="welcome"))
+        self.sm.add_widget(SignInScreen(name="signin"))
+        self.sm.add_widget(SignUpScreen(name="signup"))
+        self.sm.add_widget(MainScreen(name="main"))
+        self.sm.add_widget(SettingsScreen(name="settings"))
+        self.sm.add_widget(LanguageScreen(name="language"))
+        self.sm.current = "welcome"
+        return self.sm
+
+    def navigate_to(self, screen_name, transition_direction):
+        self.sm.transition.direction = transition_direction
+        self.sm.current = screen_name
+
+    def open_settings_menu(self, **kwargs):
+        self.navigate_to("settings", "left")
+
+    def main_menu(self, **kwargs):
+        if self.transition_type == "settings":
+            self.navigate_to("main", "right")
+        elif self.transition_type == "language":
+            self.navigate_to("main", "up")
+
+    def language_menu(self, **kwargs):
+        self.transition_type = "language"
+        self.navigate_to("language", "down")
+
+    def sign_in_menu(self, **kwargs):
+        self.navigate_to("signin", "right")
+
+    def sign_up_menu(self, **kwargs):
+        self.navigate_to("signup", "right")
+
+    def welcome_menu(self, **kwargs):
+        self.navigate_to("welcome", "left")        
         self.sm.current = "welcome"
 
 
