4d23c150431403c6c78f9bb0247ed53273eadc8f
MariusAngermann08 <marius.angermann.2018@gmail.com>
2025-05-24 14:05:01 +0200

    Updated install.py

--- install.py
+++ install.py
@@ -1,9 +1,14 @@
 #Install all needed modules from requirements.txt via pip
 import subprocess
 import sys
-import os
 
+
+#Install all needed modules from requirements.txt via pip
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 print("All needed modules installed")
 
+print("Installation finished")
+
+#Wait for user to press enter
+input("Press Enter to exit...")
